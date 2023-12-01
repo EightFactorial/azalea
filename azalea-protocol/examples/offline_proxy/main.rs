f15c8b2116b3a449d74d24ea0e0591aec4e100f2
EightFactorial <29801334+EightFactorial@users.noreply.github.com>
2023-12-01 11:51:16 -0800

    Compact formatting

--- azalea-protocol/examples/offline_proxy/main.rs
+++ azalea-protocol/examples/offline_proxy/main.rs
@@ -29,6 +29,7 @@ async fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt()
         .with_max_level(Level::INFO)
         .without_time()
+        .compact()
         .init();
 
     // Bind to an address and port
